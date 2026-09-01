5afb2dbdd49df0f78e6260cbe226af8c69f3c168
Farick <farickterancusi@gmail.com>
2026-09-01 13:22:24 -0500

    Mejorado PlayerController y documentada la logica

--- Assets/_Proyect/Scripts/PlayerController.cs
+++ Assets/_Proyect/Scripts/PlayerController.cs
@@ -1,23 +1,27 @@
 using TMPro;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5;
+    [Header("Movimiento")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5.5f;
+
+    [Header("Detección del suelo")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundRadius = 0.1f;
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("Interfaz")]
+    [SerializeField] private TMP_Text textCoins;
+
     private Rigidbody2D rb2D;
     private float move;
-
-    public float jumpForce = 4;
     private bool isGrounded;
-    public Transform groundCheck;
-    public float groundRadius = 0.1f;
-    public LayerMask groundLayer;
     private Animator animator;
     private int coins;
-    public TMP_Text textCoins;
-    
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
@@ -26,30 +30,39 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        move= Input.GetAxisRaw("Horizontal");
-        rb2D.linearVelocity = new Vector2(move*speed,rb2D.linearVelocity.y);
+        // Obtiene el movimiento horizontal y aplica una respuesta inmediata.
+        move = Input.GetAxisRaw("Horizontal");
+        rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
 
-        if(move != 0)
-           transform.localScale = new Vector3(Mathf.Sign(move),1,1);
+        // Orienta al personaje según la dirección del movimiento.
+        if (move != 0)
+            transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        // Solo permite saltar cuando el personaje está sobre el suelo.
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb2D.linearVelocity= new Vector2(rb2D.linearVelocity.x, jumpForce);
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
         }
 
+        // Actualiza los parámetros utilizados por las animaciones.
         animator.SetFloat("Speed", Mathf.Abs(move));
         animator.SetFloat("VerticalVelocity", rb2D.linearVelocity.y);
         animator.SetBool("IsGrounded", isGrounded);
-
     }
 
     void FixedUpdate()
     {
-        isGrounded= Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+        // Comprueba si existe suelo debajo del personaje.
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundRadius,
+            groundLayer
+        );
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        // Recoge la moneda y actualiza el contador.
         if (collision.transform.CompareTag("Coin"))
         {
             Destroy(collision.gameObject);
@@ -57,25 +70,30 @@ public class PlayerController : MonoBehaviour
             textCoins.text = coins.ToString();
         }
 
+        // Al tocar los pinchos, reinicia la escena.
         if (collision.transform.CompareTag("Spikes"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
+        // Al tocar un barril, el jugador recibe un impulso hacia atrás.
         if (collision.transform.CompareTag("Barrel"))
         {
-            Vector2 knockbackDir = (rb2D.position-(Vector2)collision.transform.position).normalized;
-            rb2D.linearVelocity = Vector2.zero;
-            rb2D.AddForce(knockbackDir*3,ForceMode2D.Impulse);
+            Vector2 knockbackDir =
+                (rb2D.position - (Vector2)collision.transform.position).normalized;
 
-            BoxCollider2D[] colliders = collision.gameObject.GetComponents<BoxCollider2D>();
+            rb2D.linearVelocity = Vector2.zero;
+            rb2D.AddForce(knockbackDir * 3, ForceMode2D.Impulse);
+
+            BoxCollider2D[] colliders =
+                collision.gameObject.GetComponents<BoxCollider2D>();
 
             foreach (BoxCollider2D col in colliders)
             {
                 col.enabled = false;
             }
 
-            collision.GetComponent<Animator>().enabled=true;
+            collision.GetComponent<Animator>().enabled = true;
             Destroy(collision.gameObject, 0.5f);
         }
     }
